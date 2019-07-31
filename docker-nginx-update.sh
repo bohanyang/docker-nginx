8f30338e7858139ffb8840237b8eddf9a050a5f8
Bohan Yang <youthdna@live.com>
2019-07-31 19:36:42 +0800

    Add comments

--- docker-nginx-update.sh
+++ docker-nginx-update.sh
@@ -3,14 +3,22 @@
 set -e
 
 destdir="/etc/nginx"
+
+# First Start: "/etc/nginx"
+# Subsequent Updates: "/usr/src/docker-nginx/v0000000000000000000"
 currdir="$(readlink -f $destdir)"
+
 workdir="/usr/src/docker-nginx"
 confdir="$workdir/conf"
 defaults="$workdir/defaults"
+
+# First Start: "/usr/src/docker-nginx/v0000000000000000000"
+# Subsequent Updates: "/usr/src/docker-nginx/v1111111111111111111"
 nextdir="$workdir/v$(date +%s%N)"
 
 rm -rf "$nextdir"
 mkdir "$nextdir"
+
 mkdir -p "$confdir"
 cp -R "$defaults"/* "$nextdir"
 cp -R "$confdir"/* "$nextdir"
